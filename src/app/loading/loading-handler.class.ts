dbb7ad686e1a29d9c3bbc3c864378498e1b165cb
Jasper De Smet <jasp.desmet@gmail.com>
2018-06-20 14:43:55 +0200

    Added styling for loading component

--- src/app/loading/loading-handler.class.ts
+++ src/app/loading/loading-handler.class.ts
@@ -2,13 +2,14 @@ import { timer, Subject } from 'rxjs';
 import { map, distinctUntilChanged, delay } from 'rxjs/operators';
 
 export class LoadingHandler {
-  private isLoading$ = new Subject();
-  private timer$;
-  private status = {
+  static STATUS = {
     SHORT: 'SHORT',
     MIDDLE: 'MIDDLE',
     LONG: 'LONG',
   };
+
+  private isLoading$ = new Subject();
+  private timer$;
 
   public isLoading(): Subject<any> {
     return this.isLoading$;
@@ -19,19 +20,17 @@ export class LoadingHandler {
       .pipe(
         delay(1000), // Faster than 1 second --> do nothing
         map((i) => {
-
-          // Faster than 2 seconds --> show short loading state
-          if (i < 2) {
-            return this.status.SHORT;
-          }
-
           // Faster than 5 seconds --> show middle loading state
           if (i < 5) {
-            return this.status.MIDDLE;
+            return LoadingHandler.STATUS.SHORT;
+          }
+
+          if (i < 10) {
+            return LoadingHandler.STATUS.MIDDLE;
           }
 
           // Longer than 5 seconds --> show long loading state
-          return this.status.LONG;
+          return LoadingHandler.STATUS.LONG;
         }),
         distinctUntilChanged(),
       )
