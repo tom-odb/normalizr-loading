3290c84ce2bf50f19b8f0f17fdde6b544626082e
Tom Opdebeeck <tom.opdebeeck@district01.be>
2018-06-20 13:29:45 +0200

    implement heroes module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,59 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { select } from '@angular-redux/store';
-import { Observable } from 'rxjs';
-
-import { heroesList } from './store/heroes/selectors';
-import { HeroActions } from './store/heroes/actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  @select(heroesList) public heroes$: Observable<any>;
-  @select(['heroes']) public meta$: Observable<any>;
-  @select(['heroes', 'loading']) public heroesLoading$: Observable<any>;
-  @select(['heroes', 'pagination']) public heroesPagination$: Observable<any>;
-  @select(['heroes', 'error']) public heroesError$: Observable<any>;
-
-  public page = 1;
-  private isFetching;
-
-  constructor(
-    private heroActions: HeroActions,
-  ) { }
-
-  public ngOnInit(): void {
-    this.fetchHeroes();
-  }
-
-  public prev() {
-    if (this.page > 1) {
-      this.page = this.page > 1 ? this.page - 1 : 1;
-      this.fetchHeroes();
-    }
-  }
-
-  public next() {
-    this.page = this.page + 1;
-    this.fetchHeroes();
-  }
-
-  public fetchHeroes() {
-    this.cancelFetch();
-
-    this.isFetching = this.heroActions
-      .fetchAll({
-        offset: (this.page - 1) * 10,
-        limit: 10,
-      })
-      .subscribe();
-  }
-
-  public cancelFetch() {
-    if (this.isFetching) {
-      this.isFetching.unsubscribe();
-    }
-  }
+export class AppComponent {
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { APP_ROUTES } from './app.routes';
+import { Pages } from './pages';
+
 import { StoreModule } from './store/store.module';
 import { LoadingModule } from './loading/loading.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Pages,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(APP_ROUTES),
     StoreModule,
     LoadingModule,
   ],
